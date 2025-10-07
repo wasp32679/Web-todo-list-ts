4d76d75d9ca7c75a354115f88c3725e82322ec36
Ryan <ryan.kenmoe@outlook.fr>
2025-10-07 15:40:10 +0200

    Todo storage (#20)
    * feat: added todo input and button with task when the button is clicked
    
    * refactor: gemini review consideration
    
    * feat: added label for todo input to improve accessibility
    
    * feat: added localStorage to keep user data between page reloads

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ const addBtn = document.querySelector<HTMLButtonElement>('#add-todo-button')
 const todoInput = document.querySelector<HTMLInputElement>('#todo-input')
 const errorTxt = document.querySelector<HTMLDivElement>('#errorText')
 const tasksList = document.querySelector<HTMLUListElement>('#todo-elements')
+interface Task {
+  task: string
+}
+const arrOfTask: Task[] = []
 
 if (
   addBtn === null ||
@@ -23,12 +27,35 @@ const addTodo = () => {
     tasksList.appendChild(newTask)
     newTask.innerText = todoInput.value
     todoInput.value = ''
+    arrOfTask.push({ task: newTask.innerText })
+    const JSONTaskList = JSON.stringify(arrOfTask)
+    localStorage.setItem('taskList', JSONTaskList)
   }
 }
+const storedTaskListStr = localStorage.getItem('taskList')
+const storedTaskListArr: Task[] = storedTaskListStr
+  ? JSON.parse(storedTaskListStr)
+  : []
 
-addBtn.addEventListener('click', addTodo)
+addBtn.addEventListener('click', () => {
+  addTodo()
+})
 todoInput.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     addTodo()
+  }
+})
+
+window.addEventListener('load', () => {
+  if (storedTaskListArr !== null) {
+    for (let i = 0; i < storedTaskListArr.length; i++) {
+      const newTask = document.createElement('li')
+      newTask.setAttribute('class', 'task border')
+      tasksList.appendChild(newTask)
+      newTask.innerText = storedTaskListArr[i].task
+      arrOfTask.push({ task: newTask.innerText })
+      const JSONTaskList = JSON.stringify(arrOfTask)
+      localStorage.setItem('taskList', JSONTaskList)
+    }
   }
 })
