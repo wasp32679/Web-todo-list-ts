4fceedbfa60706ad8505b5baa8674f712c038a6f
wasp32679 <ryan.kenmoe@outlook.fr>
2025-10-07 09:20:02 +0200

    refactor: gemini review consideration

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import './style.css'
 const addBtn = document.querySelector<HTMLButtonElement>('#add-todo-button')
 const todoInput = document.querySelector<HTMLInputElement>('#todo-input')
 const errorTxt = document.querySelector<HTMLDivElement>('#errorText')
-const tasksLi = document.querySelector<HTMLUListElement>('#todo-elements')
+const tasksList = document.querySelector<HTMLUListElement>('#todo-elements')
 
 if (
   addBtn === null ||
   todoInput === null ||
   errorTxt === null ||
-  tasksLi === null
+  tasksList === null
 ) {
   throw new Error('Missing variables for app to start')
 }
@@ -18,11 +18,11 @@ const addTodo = () => {
     errorTxt.innerText = 'Error input is empty'
   } else {
     errorTxt.innerText = ''
-    const newTask = document.createElement('div')
-    newTask.setAttribute('id', 'task')
-    newTask.setAttribute('class', 'border')
-    tasksLi.appendChild(newTask)
+    const newTask = document.createElement('li')
+    newTask.setAttribute('class', 'task border')
+    tasksList.appendChild(newTask)
     newTask.innerText = todoInput.value
+    todoInput.value = ''
   }
 }
 
