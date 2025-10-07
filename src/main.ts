6cbfdcdcde1ada6f38d678be409b714b145ee21b
Ryan <ryan.kenmoe@outlook.fr>
2025-10-07 09:46:28 +0200

    feat: added todo input and button with task when the button is clicked (#19)
    closes: #2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
 import './style.css'
 
-console.log('Hello from typescript')
+const addBtn = document.querySelector<HTMLButtonElement>('#add-todo-button')
+const todoInput = document.querySelector<HTMLInputElement>('#todo-input')
+const errorTxt = document.querySelector<HTMLDivElement>('#errorText')
+const tasksList = document.querySelector<HTMLUListElement>('#todo-elements')
+
+if (
+  addBtn === null ||
+  todoInput === null ||
+  errorTxt === null ||
+  tasksList === null
+) {
+  throw new Error('Missing variables for app to start')
+}
+const addTodo = () => {
+  if (todoInput.value.trim() === '') {
+    errorTxt.innerText = 'Can not add an empty task.'
+  } else {
+    errorTxt.innerText = ''
+    const newTask = document.createElement('li')
+    newTask.setAttribute('class', 'task border')
+    tasksList.appendChild(newTask)
+    newTask.innerText = todoInput.value
+    todoInput.value = ''
+  }
+}
+
+addBtn.addEventListener('click', addTodo)
+todoInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    addTodo()
+  }
+})
